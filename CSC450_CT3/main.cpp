0f80cb8017087ba97efcd07a0534aeee8da559cb
Paul Bauer <waaa.up@gmail.com>
2023-10-26 21:45:55 -0500

    Initial Commit

---
+++ CSC450_CT3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CSC450_CT3
+//
+//  Created by Paul Bauer on 10/26/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
